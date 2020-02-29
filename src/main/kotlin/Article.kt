2347f62e2ef190f7fc99ae7e652b5a6f7778f592
Raghav Khanna <rk4718@ic.ac.uk>
2020-02-29 19:05:15 +0000

    Parse csv into objects

--- src/main/kotlin/Article.kt
+++ src/main/kotlin/Article.kt
@@ -1,16 +1,12 @@
 data class Article(
     val id: Int,
     val headline: String,
-    val cost: Int,
-    val yesInf: Int,
-    val noInf: Int,
-    val yesEco: Int,
-    val noEco: Int,
+    val yesInf: Double,
+    val noInf: Double,
+    val yesEco: Double,
+    val noEco: Double,
     val isFake: Boolean = false,
     val actionMessage: String,
     val longTerm : Boolean = false
 ) {
-    override fun toString(): String {
-        return headline
-    }
 }
--- src/main/kotlin/MainClient.kt
+++ src/main/kotlin/MainClient.kt
@@ -1,7 +1,48 @@
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import java.io.BufferedReader
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
 fun main() {
     val client = Client()
-    for (i in 1..100) {
-        client.send("Hello World $i\n")
+    val Path = "healthHack.csv"
+    // read the file
+    val reader: BufferedReader = Files.newBufferedReader(Paths.get(Path))
+    // parse the file into csv values
+    val csvParser = CSVParser(reader, CSVFormat.DEFAULT)
+    for (csvRecord in csvParser) {
+
+        // Accessing Values by Column Index
+        val id = csvRecord.get(0)
+        val news = csvRecord.get(1)
+        val fake = csvRecord.get(2)
+        val noInf = csvRecord.get(3)
+        val noEco = csvRecord.get(4)
+        val yesInf = csvRecord.get(5)
+        val yesEco = csvRecord.get(6)
+        val longterm = false
+        val actionMessage = ""
+        // print the value to console
+        System.out.println("Record No - " + csvRecord.getRecordNumber())
+        var newsList = News()
+        val article = Article(
+            id = id.toInt(),
+            headline = news,
+            isFake = fake.toBoolean(),
+            noInf = noInf.toDouble(),
+            noEco = noEco.toDouble(),
+            yesEco = yesEco.toDouble(),
+            yesInf = yesInf.toDouble(),
+            actionMessage = actionMessage,
+            longTerm = longterm
+        )
+        newsList.addArticle(article)
+        client.send("---------------\n")
+        client.send("$article")
+        client.send("---------------\n")
     }
+
     client.close()
 }
